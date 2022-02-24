bb93615bd8aedf2bf216077cbbb0a171eeb34a82
Anna Kravchenko <flamedance.ann@gmail.com>
2022-02-24 15:07:15 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let brackets = bracketsConfig.map(item => item.join(''));
+
+  for (let i = 0; i < brackets.length; i++) {
+
+    if( str.includes(brackets[i]) ) {
+      str = str.replace(brackets[i], '');
+      i = -1;
+    }
+  }
+
+  return str.length == 0 ? true : false
 }
